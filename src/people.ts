fe5e36a44d71cc7e7a567f61767af961a553111f
Mykolas Raudzius <raudziusmykolas@gmail.com>
2022-08-23 19:49:03 +0300

    feat(5): eslint configuration

--- src/people.ts
+++ src/people.ts
@@ -1,8 +1,8 @@
-import { Person } from "./types/person";
+import { Person } from './types/person';
 
 const people: Person[] = [
-    { name: 'Serbentautas', surname: 'Bordiuras' },
-    { name: 'Penediktas', surname: 'Tusinis' },
+  { name: 'Serbentautas', surname: 'Bordiuras' },
+  { name: 'Penediktas', surname: 'Tusinis' },
 ];
 
 export default people;
